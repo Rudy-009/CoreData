e43c19fe0a4d520974dc4713421a578f42cb5066
Rudy-009 <rofxnaos@gmail.com>
2025-10-07 12:41:33 +0900

    키보드 내릴 수 있는 기능 추가

---
+++ CoreDataDemo-UIKit/CoreDataDemo-UIKit/Extensions/UIViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController.swift
+//  CoreDataDemo-UIKit
+//
+//  Created by 이승준 on 10/7/25.
+//
+
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
